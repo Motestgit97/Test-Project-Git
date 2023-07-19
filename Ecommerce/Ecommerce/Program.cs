7f7b9e8244fdc3a76ca055e19adff0ab5158458b
mohamed <mohamedhakk67@gmail.com>
2023-07-19 14:22:56 +0200

    Created the main project

---
+++ Ecommerce/Ecommerce/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
